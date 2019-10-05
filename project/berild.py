e083f87359b8b966f79177b49c5aed10a5932e43
Berild <moutzebe@gmail.com>
2019-10-05 17:40:42 +0200

    add neural network, naive bayes and logreg

--- project/berild.py
+++ project/berild.py
@@ -5,48 +5,16 @@
 
 # In[49]:
 
-
 import pandas as pd
 import numpy as np
-from sklearn.metrics import mean_squared_error, roc_auc_score, roc_curve
+import pickle
+from sklearn.metrics import mean_squared_error, roc_auc_score, roc_curve, confusion_matrix
 
 
-train_df = pd.read_csv("data/train_small.csv", header=None)
-test_df = pd.read_csv("data/test_small.csv")
+train_df = pd.read_csv("project/data/train_small.csv", header=None)
+test_df = pd.read_csv("project/data/test_small.csv")
 
 train_df.columns = list([test_df.columns[0],"target",*test_df.columns[1:]])
-
-
-# ## Visualization
-
-# In[52]:
-
-
-train_df.head()
-
-
-# In[53]:
-
-
-test_df.head()
-
-
-# In[62]:
-
-
-train_df.iloc[:,2:].describe()
-
-
-# In[61]:
-
-
-test_df.describe()
-
-
-# ## Data Preprocessing
-
-# In[69]:
-
 
 response = np.array(train_df['target'])
 features = np.array(train_df.iloc[:,2:])
@@ -56,89 +24,26 @@ from sklearn.model_selection import train_test_split
 train_features, test_features, train_response, test_response = train_test_split(features, response, test_size = 0.25, random_state = 42)
 
 
-# ## Feature Selection 
-
-# In[70]:
-
-
-from sklearn.ensemble import RandomForestRegressor
-
-
-
-# ## Hyperparameter Tuning
-
-# In[71]:
-
-
-def evaluate(model, test_features, test_labels):
-    predictions = model.predict(test_features)
-    errors = abs(predictions - test_labels)
-    mape = 100 * np.mean(errors / test_labels)
-    accuracy = 100 - mape
-    print('Model Performance')
-    print('Average Error: {:0.4f} degrees.'.format(np.mean(errors)))
-    print('Accuracy = {:0.2f}%.'.format(accuracy))
-    
-    return accuracy
-
-
-# ## Random search
-
 # In[72]:
 
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestRegressor
+# Create the parameter grid based on the results of random search 
+param_grid = {
+    'bootstrap': [False],
+    'max_depth': [20, 30, 40],
+    'max_features': [2, 3],
+    'min_samples_leaf': [4, 5, 6],
+    'min_samples_split': [2, 3, 4],
+    'n_estimators': [1800, 2000, 2200]
+}
 
-from sklearn.model_selection import RandomizedSearchCV
-# Number of trees in random forest
-n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
-# Number of features to consider at every split
-max_features = ['auto', 'sqrt']
-# Maximum number of levels in tree
-max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
-max_depth.append(None)
-# Minimum number of samples required to split a node
-min_samples_split = [2, 5, 10]
-# Minimum number of samples required at each leaf node
-min_samples_leaf = [1, 2, 4]
-# Method of selecting samples for training each tree
-bootstrap = [True, False]
-
-# Create the random grid
-random_grid = {'n_estimators': n_estimators,
-               'max_features': max_features,
-               'max_depth': max_depth,
-               'min_samples_split': min_samples_split,
-               'min_samples_leaf': min_samples_leaf,
-               'bootstrap': bootstrap}
-
-# First create the base model to tune
+# Create a base model
 rf = RandomForestRegressor(random_state = 42)
-# Random search of parameters, using 3 fold cross validation, 
-# search across 100 different combinations, and use all available cores
-rf_random = RandomizedSearchCV(estimator=rf, param_distributions=random_grid,
-                              n_iter = 100, scoring='neg_mean_absolute_error', 
-                              cv = 3, verbose=2, random_state=42, n_jobs=-1,
-                              return_train_score=True)
 
-# Fit the random search model
-rf_random.fit(train_features, train_response);
-
-
-# In[ ]:
-
-
-rf_random.best_params_
-
-
-# In[ ]:
-
-
-base_model = RandomForestRegressor(n_estimators = 10, random_state = 42)
-base_model.fit(train_features, train_response)
-base_accuracy = evaluate(base_model, test_features, test_response)
-
-
-# In[ ]:
-
-
-best_random = rf_random.best_estimator_
-random_accuracy = evaluate(best_random, test_features, test_response)
+# Instantiate the grid search model
+grid_search = GridSearchCV(estimator = rf, param_grid = param_grid, 
+                          cv = 3, n_jobs = -1, verbose = 2, return_train_score=True)
+grid_search.fit(train_features, train_response)
+pickle.dump(grid_search.best_params_, open('best_params_g1.pkl',  'wb'))
+pickle.dump(grid_search.best_estimator_, open('best_estimator_g1.pkl',  'wb'))
