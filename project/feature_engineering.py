f97d7bc8aab2e8aee6c928f8a30522774b556efc
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2019-10-20 13:28:08 +0200

    feature engineering: Make function wrapper

--- project/feature_engineering.py
+++ project/feature_engineering.py
@@ -1,4 +1,8 @@
+from multiprocessing import Pool
+
 import pandas as pd
+
+import datasets as ds
 
 
 def categorical_feature(args):
@@ -11,12 +15,20 @@ def categorical_feature(args):
             matches[index] = False
         unique_in_train = not matches.any()
         if unique_in_train:
-            unique_in_test = row in test_rows
+            test_matches = (test_rows == row)
+            if not is_train:
+                test_matches[index] = False
+            unique_in_test = not test_matches.any()
+
             if unique_in_test:
                 new_column.append(4)
             else:
                 new_column.append(3)
             continue
+
+        if is_train:
+            matches[index] = True
+
         targets = train_targets[matches]
         contains_zeros = 0 in targets
         contains_ones = 1 in targets
@@ -37,3 +49,57 @@ def categorical_feature(args):
             ordered=False,
         ),
     )
+
+
+def categorical_uniqueness(dataset):
+    train = dataset.all_train
+    test = dataset.unlabeled_test
+
+    train_iterable = zip(
+        train.X.columns,
+        train.X.to_numpy().T,
+        train.X.to_numpy().T,
+        test.X.to_numpy().T,
+        [train.y.to_numpy()] * 200,
+        [True] * 200,
+    )
+    pool = Pool()
+    new_train_columns = {}
+    for result in pool.imap(categorical_feature, train_iterable):
+        print(f"Finished processing: {result[0]} (TRAIN)", end="\r")
+        new_train_columns[result[0]] = result[1]
+
+    print()
+    test_iterable = zip(
+        train.X.columns,
+        test.X.to_numpy().T,
+        train.X.to_numpy().T,
+        test.X.to_numpy().T,
+        [train.y.to_numpy()] * 200,
+        [False] * 200,
+    )
+    pool = Pool()
+    new_test_columns = {}
+    for result in pool.imap(categorical_feature, test_iterable):
+        print(f"Finished processing: {result[0]} (TEST)", end="\r")
+        new_test_columns[result[0]] = result[1]
+
+    new_train = train.X.copy()
+    for column_name, values in new_train_columns.items():
+        new_train[column_name] = pd.Categorical(
+            values,
+            categories=[0, 1, 2, 3, 4],
+            ordered=False,
+        )
+    new_train["target"] = train.y.copy()
+
+    new_test = test.X.copy()
+    for column_name, values in new_test_columns.items():
+        new_test[column_name] = pd.Categorical(
+            values,
+            categories=[0, 1, 2, 3, 4],
+            ordered=False,
+        )
+
+    engineered_dataset = ds.DataSet(train=new_train, test=new_test)
+    return engineered_dataset
